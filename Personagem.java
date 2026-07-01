e0e2469f0cf4e6cd25b75d1172d8a4588f12621d
Gabriel Merege <144845580+HOOKAI512@users.noreply.github.com>
2026-06-30 21:41:16 -0300

    Refactor Personagem class and remove comments

--- Personagem.java
+++ Personagem.java
@@ -1,58 +1,51 @@
-package eldoria;
-
-import java.util.Objects;
-
-/**
- * Classe base para representar personagens do reino de Eldoria.
- * Aplica conceitos de encapsulamento, herança e polimorfismo.
- */
-public class abstract Personagem {
-    // Atributos privados (encapsulamento)
+public abstract class Personagem { 
+    private String nome;
+    private String classe;
+    private int vida;
+    private double poderDeAtaque;
+    private int nivel;
 
 
-    // * Construtor que inicializa todos os atributos do personagem.
-
-    // Getters
-
-    // Setters com validações
-
-    // * Criar métodos que define os pontos de vida do personagem.
-
-
-    /**
-     * Criar método que define a habilidade do personagem.
-     * Deve ser sobrescrito pelas subclasses para implementar comportamentos específicos.
-     */
-
-
-
-    /**
-     * Sobrescrita do método toString() para exibir informações do personagem.
-     * @return String formatada com todos os atributos do personagem
-     */
-    @Override
-    public String toString() {
-
+    public Personagem(String nome, String classe, int vida, double poderDeAtaque, int nivel) {
+        this.nome = nome;
+        this.classe = classe;
+        this.vida = vida;
+        this.poderDeAtaque = poderDeAtaque;
+        this.nivel = nivel;
     }
 
-    /**
-     * Sobrescrita do método equals() para comparar personagens.
-     * Dois personagens são considerados iguais se tiverem o mesmo nome e classe.
-     * @param obj Objeto a ser comparado
-     * @return true se os personagens forem iguais, false caso contrário
-     */
-    @Override
-    public boolean equals(Object obj) {
-
+    public String getNome() {
+        return nome;
     }
 
-    /**
-     * Sobrescrita do método hashCode() para ser consistente com equals().
-     * @return Código hash baseado no nome e classe
-     */
-    @Override
-    public int hashCode() {
-
+    public String getClasse() {
+        return classe;
     }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public double getPoderDeAtaque() {
+        return poderDeAtaque;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public void exibirStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Vida: " + vida);
+        System.out.println("Poder de Ataque: " + poderDeAtaque);
+        System.out.println("Nível: " + nivel);
+    }
+
+    protected abstract void usarHabilidade();
+
 }
-
