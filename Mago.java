02d04240f9bfe7f1c23ceb0f5fef34e1166110a2
Gabriel Merege <144845580+HOOKAI512@users.noreply.github.com>
2026-06-30 21:43:14 -0300

    Enhance Mago class with mana management
    Added mana attribute and related methods to Mago class.

--- Mago.java
+++ Mago.java
@@ -1,23 +1,27 @@
-package eldoria;
-
-/**
- * Classe que representa um Mago no reino de Eldoria.
- * Herda de Personagem e implementa habilidades específicas de magia.
- */
 public class Mago extends Personagem {
+    private int mana;
 
-    
-    // * Construtor para criar um Mago.
-
-
-
-    /**
-     * Sobrescrita do método usarHabilidade() específica para Magos.
-     * Demonstra polimorfismo e vinculação dinâmica.
-     */
-    @Override
-    public void usarHabilidade() {
-
+    public Mago(String nome, int vida, double poderDeAtaque, int nivel, int mana) {
+        super(nome, "Mago", vida, poderDeAtaque, nivel);
+        this.mana = mana;
     }
-}
 
+    public void usarHabilidade() {
+        System.out.println(getNome() + " Conjura Amaterasu!");
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    @Override
+    public void exibirStatus() {
+        super.exibirStatus();
+        System.out.println("Mana: " + mana);
+    }
+
+}
