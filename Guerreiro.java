a2272a0b4c7416c06bcee282a81315dfb81b0624
Gabriel Merege <144845580+HOOKAI512@users.noreply.github.com>
2026-06-30 21:44:06 -0300

    Add energy attribute and methods to Guerreiro class

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,20 +1,27 @@
-package eldoria;
-
-/**
- * Classe que representa um Guerreiro no reino de Eldoria.
- * Herda de Personagem e implementa habilidades específicas de combate.
- */
 public class Guerreiro extends Personagem {
+    private int energia;
 
-     // * Construtor para criar um Guerreiro.
-
-    /**
-     * Sobrescrita do método usarHabilidade() específica para Guerreiros.
-     * Demonstra polimorfismo e vinculação dinâmica.
-     */
-    @Override
-    public void usarHabilidade() {
-
+    public Guerreiro(String nome, int vida, double poderDeAtaque, int nivel, int energia) {
+        super(nome, "Guerreiro", vida, poderDeAtaque, nivel);
+        this.energia = energia;
     }
-}
 
+    public void usarHabilidade() {
+        System.out.println(getNome() + " Ataca com sua espada!");
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+    @Override
+    public void exibirStatus() {
+        super.exibirStatus();
+        System.out.println("Energia: " + energia);
+    }
+
+}
